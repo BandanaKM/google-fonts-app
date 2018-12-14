9149dcf5b5d03bf259c1f7b2a037f0a6ad976a1c
Bana Malik <bandanakm@gmail.com>
2018-12-14 17:28:36 -0500

    add left panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import LeftPanel from './LeftPanel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+         <div>Hi</div>
+         <LeftPanel />
       </div>
     );
   }
---
+++ src/LeftPanel/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class LeftPanel extends Component {
+  render() {
+    return (
+      <div className="LeftPanel">
+        <p>left panel</p>
+      </div>
+    )
+  }
+}
+
+export default LeftPanel;
