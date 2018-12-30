9943d8fdb706dc6ddfd00abbefa4fb98116f2b82
Bana Malik <bandanakm@gmail.com>
2018-12-30 15:43:03 -0500

    add color picker when user clicks button

--- src/RightPanel/ColorPicker/index.js
+++ src/RightPanel/ColorPicker/index.js
@@ -3,19 +3,33 @@ import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color';
 
 class ColorPicker extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      displayColorPicker: false,
+    }
+  }
+
+  toggleDisplay = () => {
+    this.setState({
+      displayColorPicker: !this.state.displayColorPicker
+    })
+  }
+
   render() {
     const { textColor } = this.props;
     return (
       <div>
         <h4>{textColor ? 'Text Color' : 'Background Color'}</h4>
-        <SketchPicker />
+        {this.state.displayColorPicker && <SketchPicker />}
+        <button onClick={this.toggleDisplay}>Go</button>
       </div>
     )
   }
 }
 
-export default ColorPicker;
-
 ColorPicker.propTypes = {
   textColor: PropTypes.string,
 }
+
+export default ColorPicker;
