bb7f3a96191ce7e6a678635e7982eaf549bc94fc
Bana Malik <bandanakm@gmail.com>
2018-12-29 22:29:21 -0500

    add font family

--- src/RightPanel/FontFamily/index.js
+++ src/RightPanel/FontFamily/index.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 
+const options = [
+  {
+   id: "arial",
+   displayText: "arial"
+  },
+  {
+   id: "times new roman",
+   displayText: "times new roman"
+  },
+  {id: "garamond",
+   displayText: "garamond"
+  },
+]
 
 class FontFamily extends Component {
+  renderOption = (id, selectedOption) => (
+    <option value={id} selected={selectedOption === id}>{id}</option>
+  )
   render() {
+    const { activeFormEntry, setFontFamily } = this.props;
+    const selectedOption = activeFormEntry.fontFamily || "arial";
     return(
       <div className="classification">
         <h4>Font Family</h4>
-        <select name="classification" className="classificationSelect">
-        <option value="arial">Arial</option>
-        <option value="times">Times New Roman</option>
-        <option value="garamond">Garamond</option>
+        <select name="classification" className="classificationSelect" onChange={e => setFontFamily(e.target.value)}>
+        {options.map(option => this.renderOption(option.id, selectedOption))}
         </select>
       </div>
     );
   }
+}
+
+FontFamily.propTypes = {
+  activeFormEntry: PropTypes.object,
+  setFontFamily: PropTypes.func,
 }
 
 export default FontFamily;
